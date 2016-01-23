c4ad43ca5e4f4080da4db46cfbab20113870c319
Tom Coakes <tomcoakes@gmail.com>
2016-01-23 19:39:44 +0000

    add npm start script that uses --harmony flag

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const http = require('http');
 
+const port = 1337;
+const hostname = '127.0.0.1';
+
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Movie Database App');
-}).listen(1337, '127.0.0.1', () => {
+}).listen(port, hostname, () => {
   console.log('The server is up. The magic happens on port 1337');
 });
