5d70c9e106cdc47e85ef08ddedf96fb035866a77
Tom Coakes <tomcoakes@gmail.com>
2016-01-24 11:42:42 +0000

    nightwatch runs an example test

---
+++ tests/features/app.spec.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'Demo test for Google' : function(browser) {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[type=text]', 'Tom Coakes')
+      .waitForElementVisible('button[name=btnG]', 1000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .assert.containsText('#main', 'Tom Coakes')
+      .end();
+  }
+};
