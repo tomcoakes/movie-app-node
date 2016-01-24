d94778cf7d63b8c89511fbc1d3a9b60608232374
Tom Coakes <tomcoakes@gmail.com>
2016-01-24 14:40:15 +0000

    log port number on server startup

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+const port = 3000;
+
 app.get('/', (req, res) => {
   res.send('Movie Database App');
 });
 
-app.listen(3000);
+app.listen(port, function() {
+  console.log(`You can access the server on port: ${port}`);
+});
