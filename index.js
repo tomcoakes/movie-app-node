701bd9683cea2589da5e4d1d8396ddb8d0129891
Tom Coakes <tomcoakes@gmail.com>
2016-01-26 13:55:25 +0000

    npm test runs grunt featureTests

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Movie Databaseh App');
+  res.send('Movie Database App');
 });
 
 app.listen(port, function() {
