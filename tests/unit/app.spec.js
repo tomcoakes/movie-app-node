51b0ec86f766bd5496a2ebb41ac411faf7d8f8e5
Tom Coakes <tomcoakes@gmail.com>
2016-02-07 00:36:21 +0000

    add mocha and pass placeholder test

---
+++ tests/unit/app.spec.js
@@ -0,0 +1,9 @@
+const app = '../../server.js';
+
+const expect = require('chai').expect;
+
+describe('first test', () => {
+  it('true is true', () => {
+    expect(true).to.equal(true);
+  });
+});
