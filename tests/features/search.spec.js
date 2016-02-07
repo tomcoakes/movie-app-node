eb9a4bc8efeaf0067e07d3fd1ba641d3d4da45d8
Tom Coakes <tomcoakes@gmail.com>
2016-02-07 11:12:42 +0000

    change test for accessing movie pages

--- tests/features/search.spec.js
+++ tests/features/search.spec.js
@@ -1,7 +1,7 @@
 module.exports = {
-  'Searching for Children of Men' : function(browser) {
+  'Find movie by title' : function(browser) {
     browser
-      .url('http://localhost:3000/search?q=childrenofmen')
+      .url('http://localhost:3000/movie/childrenofmen')
       .assert.containsText('body', 'Children of Men')
       .end();
   }
