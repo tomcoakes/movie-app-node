dbb79658f75415fbf3aa6314e1f96c778302aee0
Tom Coakes <tomcoakes@gmail.com>
2016-02-08 17:33:15 +0000

    server instantiates new instance of routes

--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const routes = require('./routes')();
+const Routes = require('./routes');
+const routes = new Routes();
+
 const viewsPath = path.resolve('src/client/views');
 
 module.exports = () => {
