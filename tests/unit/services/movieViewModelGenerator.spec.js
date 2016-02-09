9164d60adf066e51460ab40d4c3afee82869ae15
Tom Coakes <tomcoakes@gmail.com>
2016-02-09 23:51:36 +0000

    add test for movieViewModelGenerator

--- tests/unit/services/movieViewModelGenerator.spec.js
+++ tests/unit/services/movieViewModelGenerator.spec.js
@@ -1,7 +1,11 @@
+const expect = require('chai').expect;
+
 const generateMovieViewModel = require('../../../src/server/services/movieViewModelGenerator');
+const omdbapiResponse = require('../../fixtures/movieService.response');
+
 
 describe('viewModelGenerator generates a model that', () => {
   it('includes a movieTitle', () => {
-
+      expect(generateMovieViewModel(omdbapiResponse).movieTitle).to.equal('Inception');
   });
 });
